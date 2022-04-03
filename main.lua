f46c9d144fc583fc4a76cb992a0f9c025ce8b907
ToStupid <92023248+Feenzyx@users.noreply.github.com>
2022-04-04 01:22:27 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local config = require(script.Parent.Config)
 local economie = config.GeldSysteem
 local frame = game.StarterGui["fx-cardealer"].ScrollingFrame
 local source
-local keys = "lol"
+local keys = "lol" or "lolll"
 
 wait(8)
 
