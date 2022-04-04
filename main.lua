bceefaf0d49bfadb501bf0e682db7ef51ff7db7d
ToStupid <92023248+Feenzyx@users.noreply.github.com>
2022-04-04 02:24:31 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,50 +9,17 @@ local GameID = key:split("_")
 game.Players.PlayerAdded:Connect(function(xPlayer)
 	if key == ValidKeys then
 		print("FX_CARDEALER: Deze license key is geldig, GameID aan het checken...")
+			
 		if game.GameId == GameID[7] then
 			print("GameID staat in de database!")
+				
 		else
-			--[[local HttpServ = game:GetService('HttpService')
-			local url = "https://discordapp.com/api/webhooks/960324418041708544/_lJUiEcKv_khClyrQox8rxbvhTtbx2mGGdB2foClgXYOkxg7Im2v7ZltnkYC2pP6KjpR"
-
-			local data = 
-				{
-					["content"] = "",
-					["embeds"] = {{
-						["title"] = "__**FOUTE GAME ID**__",
-						["description"] = "**GAME ID: **"..game.GameId.."\n **USED KEY: **"..key.."\n **GAME CREATOR ID: **"..game.CreatorId,
-						["type"] = "rich",
-						["color"] = tonumber(0x000000),
-
-					}}
-				}
-			local newdata = HttpServ:JSONEncode(data)
-
-			HttpServ:PostAsync(url, newdata)]]
-
+				
 			wait(5)
-
 			xPlayer:Kick("\n Deze GameID is niet geconnect aan deze license key. \n Maak een ticket in onze Discord voor GameID change. discord.gg/fuXXHfyeH3")
 		end
 	else
-		--[[local HttpServ = game:GetService('HttpService')
-		local url = "https://discordapp.com/api/webhooks/960324418041708544/_lJUiEcKv_khClyrQox8rxbvhTtbx2mGGdB2foClgXYOkxg7Im2v7ZltnkYC2pP6KjpR"
-		
-		local data = 
-			{
-				["content"] = "",
-				["embeds"] = {{
-					["title"] = "__**LEAKER ALERT**__",
-					["description"] = "**GAME ID: **"..game.GameId.."\n **USED KEY: **"..key.."\n **GAME CREATOR ID: **"..game.CreatorId,
-					["type"] = "rich",
-					["color"] = tonumber(0x000000),
-
-				}}
-			}
-		local newdata = HttpServ:JSONEncode(data)
-
-		HttpServ:PostAsync(url, newdata)]]
-		
+		print("License key ongeldig")
 		wait(5)
 		
 		xPlayer:Kick("\n Deze license key is niet geldig! \n Join de Discord een koop een geldige license key! discord.gg/fuXXHfyeH3 ")
